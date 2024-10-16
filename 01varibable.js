444f249aefe5faff2b1af21d6efff25c2c980d1e
Ritesh Rajput <159470130+riteshrajput45@users.noreply.github.com>
2024-10-16 18:16:54 +0530

    Variable

---
+++ 01varibable.js
@@ -0,0 +1,9 @@
+const accountEmail ="mrriteshRajput045gmail.com"; 
+let  accountNumber= 5451254125412451;    // let ka use hm pahle krte the ab esliye nhi krte kyuki scope ki problem aati hai
+var accountId =1234556;
+accountCity ="indore" 
+
+// accountEmail =ritesh.com
+
+console.table([{accountEmail:accountEmail},accountId,accountNumber])
+ 
