e06d8019f8d8b5534bd3e862d34308f10eb3c985
Ritesh Rajput <159470130+riteshrajput45@users.noreply.github.com>
2024-10-16 18:40:08 +0530

    datatypes

---
+++ 02datatypes.js
@@ -0,0 +1,21 @@
+// DATA TYPES
+
+let name = "ritesh" ; 
+let age = 18 
+let login = false 
+
+
+// Number 
+// bigInt 
+// Stiring
+// boolean
+//Null  (khali hain)
+// undifind 
+// symbol (uniqeless ke liye us hota)
+
+// object 
+// typeof
+// example 
+console.log(typeof age)  //number
+console.log(typeof  null)  //object
+console.log(typeof  undefined)  //undefined 
