735e2167e41b71860fed89c204aa0be0366bfe9b
Ritesh Rajput <159470130+riteshrajput45@users.noreply.github.com>
2024-10-17 11:57:28 +0530

    oprations

--- 03conversionOperations.js
+++ 03conversionOperations.js
@@ -24,3 +24,36 @@ console.log(typeof numver)
 
 const converstringNumver = String(numver);
 console.log( typeof converstringNumver);
+
+
+
+
+// *****************************operations******************//
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);//power work
+// console.log(2%2);//reminder work
+// console.log(2/2);
+
+let str1 = "ram";
+let str2 ="sita"
+
+
+console.log(2+1+"1"+2+2);
+console.log(1+2+"3");
+
+console.log(+true) //1
+console.log(+"");//0
+
+// pri fix and Post fix 
+//priii fix me value pahle  add ho jati hain
+//pro fix me value  bad me add ho ti hain
+
+// example
+let  value1 = 100;
+// let valu2 = (++value1); // prii fix
+let valu2 = (value1++); //pro fix
+console.log(valu2);
+//you can read priifix and profix  tc39.es/ecma262
