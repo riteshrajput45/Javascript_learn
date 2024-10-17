cf2da9cd23176a2e5da1b2f3243dc5d8be32f028
Ritesh Rajput <159470130+riteshrajput45@users.noreply.github.com>
2024-10-17 11:31:13 +0530

    conversion

---
+++ 03conversionOperations.js
@@ -0,0 +1,26 @@
+let Score = 264;
+
+
+console.log(typeof   Score)
+
+let convertValue = String(Score);
+console.log(typeof convertValue);
+
+//"33"=>33
+// "33abd"=>Nan
+// true=>1; false =>0
+
+let login = ""
+let login2 = "rladsa"
+
+let converBoleen =Boolean(login2)
+let converBoleen2 =Boolean(login)
+
+console.log(converBoleen);
+console.log(converBoleen2);
+
+let numver =33
+console.log(typeof numver)
+
+const converstringNumver = String(numver);
+console.log( typeof converstringNumver);
