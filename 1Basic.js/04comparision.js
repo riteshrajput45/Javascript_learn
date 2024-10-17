8028a55965c5d3a4ff297730b4c49eaeb97b6853
Ritesh Rajput <159470130+riteshrajput45@users.noreply.github.com>
2024-10-17 12:50:08 +0530

    datasumery

---
+++ 1Basic.js/04comparision.js
@@ -0,0 +1,13 @@
+// // console.log(2>1);
+// // console.log(2>=1);
+// // console.log(2==1);
+// // console.log(2!=1);
+// // console.log(2<1);
+// console.log(null>1)
+// console.log(null==1)
+// console.log(null>=1)
+
+// ====  yeh strickly check krta hain with data type
+
+console.log("2"==2); //true
+console.log("2"===2);//false
---
+++ 1Basic.js/dataypeSummery.js
@@ -0,0 +1,36 @@
+//DATA TYPES IS TWO TYPES
+// 1.primitive Data type
+// 7type this
+//   String ,Number,Boolean,null,undefined,Symbol ,BigInt
+
+// Symbol usess symbol ka hum use uniqly kiya jata hain
+
+// let value1 = Symbol("HareRam");
+// let valu2 = Symbol("HareRam");
+// console.log(valu2==value1)//false kyuki yeh uniqely work krta hain
+
+
+
+// 2.Reference Type data type
+
+// Array,Function,Object
+
+// let Arraynum = ["ritesh","arahul","ravi"]
+// console.log(typeof Arraynum);
+
+let objectnum = {
+     name:"ritesh",
+     age:23
+}
+console.log(typeof objectnum);
+
+
+let add = function(){
+    console.log("jay shree krishna")
+}
+add();
+// console.log(result);
+
+console.log(typeof add);
+
+//jo  bi refence type .ke data type he  unka hmesha data tyoe object me aaayega 
