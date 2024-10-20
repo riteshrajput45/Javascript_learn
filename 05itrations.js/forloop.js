d4ebfbbd0ae4d8a398e64547644010796eeaa423
Ritesh Rajput <159470130+riteshrajput45@users.noreply.github.com>
2024-10-20 19:03:51 +0530

    whileloop

---
+++ 05itrations.js/forloop.js
@@ -0,0 +1,51 @@
+//FOR
+
+
+// for(let index=0;index<109;index++){
+//           console.log(`Hare Krishna ${index}`);
+// }
+
+// nested loop
+
+
+// for(let i=0;i<10;i++){
+//     console.log(`hare krishna`,i);
+//     for(let j=0; j<10;j++){
+//         console.log(`hare ram ${j}, hare krishna ${i}`)
+//     }
+// }
+
+//array ki leanth;
+
+// let power =['hanumanji','krishan','shreeRam']
+
+// for(let index =0; index<power.length; index++){
+//     const element =power[index]
+//     console.log('element: ', element);
+// }
+
+
+//SECTION /** Break And Continew *************/
+
+
+//Break
+
+//yeh apni implement break kr dega   yhi sb bandh hoga re baba
+// for(let i=0; i<10; i++){
+//     if(i===5){
+//         console.log("5 is my fav");
+//         break;
+//     }
+//     console.log(`value is ${i}`);
+// }
+
+
+//continue ka kam  karne hain ki jo code he use continue kr dega or aage jaane dega ek bar skip kreha
+for(let i=0; i<10; i++){
+    if(i===5){
+        console.log("chhalo es skip krte he s");
+        continue;
+    }
+    console.log(`value is ${i}`);
+}
+
---
+++ 05itrations.js/whilelopp.js
@@ -0,0 +1,17 @@
+// let index =0;
+
+// while(index<10){
+//     console.log(`jay shree krishna  ${index}`);
+//     index =index + 2
+// }
+
+
+let score =10;
+
+do{
+    console.log(score);
+    score --
+}while(score>10)
+
+
+//esme kam pahle hota hain then condition hoti hain 
